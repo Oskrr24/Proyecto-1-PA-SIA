fce2a35737098d970f4352452d743d118b9b08fa
Oscar Ruiz <oscar.ruiz.f@mail.pucv.cl>
2024-08-22 22:46:26 -0400

    Se colocó la primera piedra

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class index{
+
+    public static void main(String[] aStrings){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("El milo agarró la pala porfin");
+        sc.close();
+    }
+    
+}
