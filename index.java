fe5361e79d23c4924a0b2d8689a7f17bb2a29dd3
Oscar Ruiz <oscar.ruiz.f@mail.pucv.cl>
2024-08-28 00:22:45 -0400

    SE REALIZARON LOS SIGUIENTES CAMBIOS: 1) se cambió el menú a la clase index 2) Se implementó la función clearScreen 3) se modificó el menú 4) se está creando el caso 1 del Postulante 5) se agregaron más atributos a la clase Postulante 6) se inicializó la tabla hash

--- index.java
+++ index.java
@@ -1,17 +1,20 @@
 //import java.util.*;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.io.*;
 
 public class index{
 
     private static Scanner sc = new Scanner(System.in);
     private static int opcion;
+    private static Postulante postulante1;
     static HashMap<String, Postulante> MapaPostulantes;
     public static void main(String[] args){
 
         MapaPostulantes = new HashMap<>();
         menu(sc);
+
     }
 
     public static void menu(Scanner sc){
@@ -19,17 +22,23 @@ public class index{
             System.out.println("Bienvenido al Sistema de Gestión de Becas");
             System.out.println("¿Qué desea hacer?");
 
+            System.out.println("VERSIÓN DEL POSTULANTE");
             System.out.println("1) Consultar Becas disponibles");
-            System.out.println("2) Realizar postulación");
+            System.out.println("2) Realizar postulación"); //LA BECA SE AGREGA AL ARRAY DE LA TABLA, PERO QUEDA COMO EN FALSE HASTA QUE SE APRUEBE
             System.out.println("3) Consultar estado de Beca(s)");
-            System.out.println("4) Realizar Ficha");
-            System.out.println("5) Salir del programa"); 
+            System.out.println("4) Realizar Ficha/Registrarse"); //AGREGAR A LA TABLA HASH
             
-            opcion = ingresarNumeroValido(sc, 1, 5);
+            System.out.println("-------------");
+
+            System.out.println("VERSION DEL ADMINISTRADOR");
+            System.out.println("5) Gestionar solicitudes");
+            System.out.println("6) Salir del programa"); 
+
+            opcion = ingresarNumeroValido(sc, 1, 6);
             switch (opcion) {
                 case 1:
+                    clearScreen();
                     System.out.println("Ha elegido la opción 1");
-
                     mostrarBecas();
                     break;
             
@@ -47,8 +56,15 @@ public class index{
                 //Método crearFicha(input)
                     break;
                 case 5:
-                    System.out.println("Ha elegido la opción 4");
+                    System.out.println("Ha elegido la opción 5");
+                    //Método gestionarSol(input)
+                break;
+
+                case 6: 
+                    clearScreen();
+                    System.out.println("Ha elegido la opción 6");
                     System.out.println("Saliendo del programa...");
+                    System.exit(1);
                 break;
             }
 
@@ -72,10 +88,41 @@ public class index{
         }
 
     }
-    
-    public static void mostrarBecas(){
-        //Imprimir todas todas las 18 becas
+
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
+    public static void mostrarBecas(){
+        clearScreen();
+        imprimirDesdeArchivo("information.txt");
+        /*IBECAS ACTUALES
+        BECA BICENTENARIO
+        BECA JUAN GOMEZ MILLAS
+        */
+    }
     
+    public static void imprimirDesdeArchivo(String archivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void crearFicha(Scanner input){
+        postulante1 = new Postulante("Juan", "Perez", 19, "21672971-5", 1, 5.0, true, 70.0, false, 600, "ninguna", false);
+        //METER POSTULANTE AL MAPA Y EVALUAR
+    }
 }  
